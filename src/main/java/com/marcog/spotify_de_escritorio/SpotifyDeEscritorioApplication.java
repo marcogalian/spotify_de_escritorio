26d7030b687117085cbc2ae56c1851b27b0785b3
marco <marcogalian@hotmail.com>
2026-01-19 12:30:06 +0100

    Creacion del proyecto spotify de escritorio

---
+++ src/main/java/com/marcog/spotify_de_escritorio/SpotifyDeEscritorioApplication.java
@@ -0,0 +1,13 @@
+package com.marcog.spotify_de_escritorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyDeEscritorioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyDeEscritorioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcog/spotify_de_escritorio/SpotifyDeEscritorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcog.spotify_de_escritorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyDeEscritorioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
