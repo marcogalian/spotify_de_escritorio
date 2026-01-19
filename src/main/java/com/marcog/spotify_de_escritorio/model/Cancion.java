30cc36e10596aa5c724c030bd79a7e3a1aa77800
marco <marcogalian@hotmail.com>
2026-01-19 18:15:29 +0100

    Agregada notacion lombok  @builder

--- src/main/java/com/marcog/spotify_de_escritorio/model/Cancion.java
+++ src/main/java/com/marcog/spotify_de_escritorio/model/Cancion.java
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Data
+@Builder
 @Document(collection = "canciones")
 @NoArgsConstructor
 @AllArgsConstructor
