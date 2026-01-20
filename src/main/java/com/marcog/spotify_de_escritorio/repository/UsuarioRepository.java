b28b51276f02721e0e8c8899fb28ba8196f90e51
marco <marcogalian@hotmail.com>
2026-01-20 17:43:05 +0100

    Creacion login, colores y logica para el logueo del usuario en una propiedad statica

--- src/main/java/com/marcog/spotify_de_escritorio/repository/UsuarioRepository.java
+++ src/main/java/com/marcog/spotify_de_escritorio/repository/UsuarioRepository.java
@@ -4,5 +4,6 @@ import com.marcog.spotify_de_escritorio.model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-
+    // Busco al usuario por email para validar el acceso
+    Usuario findByEmail(String email);
 }
